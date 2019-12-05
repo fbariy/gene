658144fe7707b87f4d38eaf64b213c8fd84f6bec
fbariy <bariy.alex@gmail.com>
2019-12-05 21:39:02 +0300

    Another refactoring

--- src/main/scala/Estimator.scala
+++ src/main/scala/Estimator.scala
@@ -1,27 +1,33 @@
+import scala.annotation.tailrec
+
 trait Estimator {
   type Board
+  def isSolution(estimate: Int): Boolean
   def estimate(board: Board): Int
 }
 
 class FirstEstimator extends Estimator {
   type Board = List[Shape]
 
+  def isSolution(estimate: Int): Boolean = estimate == 28
+
   def estimate(board: Board): Int = {
-    def compute(current: Shape, rest: Board) = {
-      val operator: (Int, Shape) => Int = (acc, other) => if (collise(current, other)) acc else acc + 1
-      rest.foldLeft(0)(operator)
-    }
-
-    def collise(current: Shape, other: Shape) = {
-      current.x == other.x ||
-      current.y == other.y ||
-      Math.abs(other.x - current.x) == Math.abs(other.y - current.y)
-    }
-
+    @tailrec
     def estimateIter(board: Board, acc: Int): Int = board match {
       case Nil => acc
       case head :: tail => estimateIter(tail, acc + compute(head, tail))
     }
     estimateIter(board, 0)
+  }
+
+  private def compute(current: Shape, rest: Board) = {
+    val operator: (Int, Shape) => Int = (acc, other) => if (collise(current, other)) acc else acc + 1
+    rest.foldLeft(0)(operator)
+  }
+
+  private def collise(current: Shape, other: Shape) = {
+    current.x == other.x ||
+      current.y == other.y ||
+      Math.abs(other.x - current.x) == Math.abs(other.y - current.y)
   }
 }
--- src/main/scala/Shape.scala
+++ src/main/scala/Shape.scala
@@ -1,5 +1,1 @@
-class Shape(val x: Int, val y: Int)
-
-object Shape {
-  def apply(x: Int, y: Int): Shape = new Shape(x, y)
-}
+case class Shape(x: Int, y: Int)
