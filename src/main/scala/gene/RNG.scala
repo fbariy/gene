ee970d84455b0ea9893f5428e04cd25aeb23028d
fbariy <bariy.alex@gmail.com>
2020-01-18 15:55:06 +0300

    Refactor RNG

--- src/main/scala/gene/RNG.scala
+++ src/main/scala/gene/RNG.scala
@@ -3,18 +3,20 @@ package gene
 case class RNG(seed: Long) {
   def nextLong(): (Long, RNG) = {
     val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
-    val nextRNG = RNG(newSeed)
-    (newSeed >>> 16, nextRNG)
+    val newRng = RNG(newSeed)
+    (newSeed >>> 16, newRng)
   }
 
-  def nextInt(): (Int, RNG) = {
-    val (number, rng) = nextLong()
-    (number.toInt, rng)
+  def nextInt(): (Int, RNG) = nextLong() match {
+    case (number, rng) => (number.toInt, rng)
   }
 
-  def nextInt(max: Int, min: Int = 0): (Int, RNG) = {
-    val (number, rng) = nextLong()
-    val numberWithinBounds = (number % max + min).toInt
-    (numberWithinBounds, rng)
+  def nextInt(max: Int, min: Int = 0): (Int, RNG) = nextLong() match {
+    case (number, rng) => ((number % max + min).toInt, rng)
+  }
+
+  def nextBool(): (Boolean, RNG) = nextInt(1) match {
+    case (1, nextRNG) => (true, nextRNG)
+    case (2, nextRNG) => (false, nextRNG)
   }
 }
