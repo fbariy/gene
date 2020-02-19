d7791fc83c7c90a66ca64645fc3c986b30ec2110
fbariy <bariy.alex@gmail.com>
2020-02-19 22:29:15 +0300

    Add final mutation implementation

--- src/main/scala/gene/Gene.scala
+++ src/main/scala/gene/Gene.scala
@@ -1,43 +1,27 @@
 package gene
 
+import RNG2._
+import State._
+
 object Gene {
   type Individual = List[Int]
   type Population = List[Individual]
 
   def main(args: Array[String]): Unit = {
-    start()
-  }
-
-  def randArgs(max: Int, min: Int)(n: Int): State[Long, List[(Boolean, Int)]] =
-    State.sequence(List.fill(n)(State.both(RNG2.bool, RNG2.int(max, min))))
-
-  def mutation(maxGene: Int, minGene: Int)(population: Population)/*: State[Long, Population]*/ = for {
-    args <- randArgs(maxGene, minGene)(population.length)
-  } yield ???
-
-  def mutationIndividual(isNeedMutate: Boolean, gene: Int)(individual: Individual): Individual =
-    ???
-
-  def start(): Unit = {
-    val board1 = List(7, 4, 1, 7, 8, 4, 2, 4)
-    val board2 = List(7, 6, 1, 3, 8, 1, 8, 4)
-    val board3 = List(7, 2, 1, 3, 1, 1, 8, 1)
-    val board4 = List(7, 2, 1, 6, 8, 2, 1, 1)
-
     // выйгрышная
     val solutionBoard = List(7, 3, 1, 6, 8, 5, 2, 4)
-
-    val population = List(board1, board2, board3, board4)
-    val estimator = new FirstEstimator
-    val rng = RNG(0)
-    val crossover = Mutator.crossover _
-    val mutation = Mutator.firstTaskMutation
-
-    val mutator = crossover
-
-    val result = Resolver.resolve(population, mutator, estimator, rng)
-    val a = 1
   }
+
+  def mutation(maxGene: Int, minGene: Int)(population: Population): State[Long, Population] =
+    sequence(population.map(individual => mutationIndividual(maxGene, minGene)(individual)))
+
+  def mutationIndividual(maxGene: Int, minGene: Int)(individual: Individual): State[Long, Individual] = for {
+    isNeedMutate <- bool
+    gene <- int(maxGene, minGene)
+    position <- lessThan(individual.size)
+  } yield
+    if (isNeedMutate) individual.updated(position, gene)
+    else individual
 }
 
 /**
--- src/main/scala/gene/Mutator.scala
+++ src/main/scala/gene/Mutator.scala
@@ -1,67 +1,11 @@
 package gene
 
-import gene.Gene.{Individual, Population}
+import gene.Gene.Individual
 
 object Mutator {
-  private def crossover(first: Individual, second: Individual, point: Int): (Individual, Individual) = {
+  def crossover(first: Individual, second: Individual, point: Int): (Individual, Individual) = {
     val (firstLeft, firstRight) = first.splitAt(point)
     val (secondLeft, secondRight) = second.splitAt(point)
     (firstLeft ++ secondRight, secondLeft ++ firstRight)
   }
-
-  def crossover(pair: (Population, RNG)): (Population, RNG) = pair match {
-    case (pop, rng) =>
-      if (0 == pop.size % 2) crossoverSlider(pop, rng)
-      else {
-        val (newPop, newRNG) = crossoverSlider(pop.init, rng)
-        (newPop :+ pop.last, newRNG)
-      }
-  }
-
-  def crossoverSlider(pop: Population, rng: RNG): (Population, RNG) = pop.sliding(2).flatMap()
-
-//  def crossover(pair: (Population, RNG)): (Population, RNG) = {
-//    val (pop, rng) = pair
-//    pop match {
-//      case first :: second :: rest =>
-//        val (number, nextRng) = rng.nextIntLessThen(first.size)
-//        val (left, right) = crossover(first, second, number)
-//        val (resultPop, resultRng) = crossover((rest, nextRng))
-//        val newPop = List(left, right) ++ resultPop
-//        (newPop, resultRng)
-//      case first :: _ => (List(first), rng)
-//      case List() => (List(), rng)
-//    }
-//  }
-
-  def mutation(minGene: Int, maxGene: Int)(pair: (Population, RNG)) = {
-    def mutationIter(pair: (Population, RNG)): (Population, RNG) = {
-      val (pop, rng) = pair
-      pop match {
-        case List() => (List(),  rng)
-        case first :: rest =>
-          //@todo: refactor
-          val mutatedIndividual = rng.nextBool() match {
-            case (false, switchRNG) => (first, switchRNG)
-            case (true, switchRNG) =>
-              val (newIndividual, mutateRNG) = mutate(minGene, maxGene)(switchRNG, first)
-              (newIndividual, mutateRNG)
-          }
-
-          val (mutated, mutateRNG) = mutatedIndividual
-          val (newPop, mutatePopRNG) = mutationIter((rest, mutateRNG))
-          (mutated :: newPop, mutatePopRNG)
-      }
-    }
-
-    def mutate(minGene: Int, maxGen: Int)(rng: RNG, individual: Individual): (Individual, RNG) = {
-      val (point, pointRNG) = rng.nextIntLessThen(individual.size)
-      val (gene, geneRNG) = pointRNG.nextInt(maxGen, minGene)
-      (individual.updated(point, gene), geneRNG)
-    }
-
-    mutationIter(pair)
-  }
-
-  def firstTaskMutation: ((Population, RNG)) => (Population, RNG) = mutation(1, 8)
 }
