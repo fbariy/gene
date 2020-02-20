89388d70c0f33b2fa147bfae1bc4c0f052fc8532
fbariy <bariy.alex@gmail.com>
2020-02-20 23:41:01 +0300

    Naive implementation of resolve method

--- src/main/scala/gene/Gene.scala
+++ src/main/scala/gene/Gene.scala
@@ -3,13 +3,26 @@ package gene
 import RNG2._
 import State._
 
+import scala.annotation.tailrec
+
 object Gene {
   type Individual = List[Int]
   type Population = List[Individual]
+  type Estimation = (Individual, Int)
+  type PopulationEstimation = List[Estimation]
 
   def main(args: Array[String]): Unit = {
     // выйгрышная
     val solutionBoard = List(7, 3, 1, 6, 8, 5, 2, 4)
+
+    val board1 = List(7, 4, 1, 7, 8, 4, 2, 4)
+    val board2 = List(7, 6, 1, 3, 8, 1, 8, 4)
+    val board3 = List(7, 2, 1, 3, 1, 1, 8, 1)
+    val board4 = List(7, 2, 1, 6, 8, 2, 1, 1)
+    val board5 = List(7, 4, 1, 7, 8, 4, 2, 4)
+
+    val result = resolver(1, List(board1, board2, board3, board5), new FirstEstimator)
+    val temp = 5
   }
 
   def crossoverIndividual(maxGene: Int, minGene: Int)(left: Individual, right: Individual): State[Long, Population] =
@@ -21,7 +34,7 @@ object Gene {
 
   def crossover(maxGene: Int, minGene: Int)(population: Population): State[Long, Population] =
     sequence(population
-      .sliding(2)
+      .sliding(2, 2)
       .map { pair => crossoverIndividual(maxGene, minGene)(pair.head, pair.last) }
       .toList)
       .map(_.flatten)
@@ -36,6 +49,26 @@ object Gene {
   } yield
     if (isNeedMutate) individual.updated(position, gene)
     else individual
+
+  def mutationFirst: Population => State[Long, Population] = mutation(8, 1)
+  def crossoverFirst: Population => State[Long, Population] = crossover(8, 1)
+
+  @tailrec
+  def resolver(seed: Long, population: Population, estimator: Estimator): Individual = {
+    val (nextSeed, result) = mutator(population, estimator).run(seed)
+    val maybeSolution = result.find(individual => estimator.isSolution(individual._2))
+
+    maybeSolution match {
+      case None => resolver(nextSeed, result.map(_._1), estimator)
+      case Some((solution, _)) => solution
+    }
+  }
+
+  def mutator(population: Population, estimator: Estimator): State[Long, PopulationEstimation] = for {
+    mutationResult <- mutationFirst(population)
+    crossoverResult <- crossoverFirst(mutationResult)
+    //@todo: add selector
+  } yield crossoverResult.map(individual => (individual, estimator.estimate(individual)))
 }
 
 /**
--- src/main/scala/gene/Selector.scala
+++ src/main/scala/gene/Selector.scala
@@ -3,7 +3,7 @@ package gene
 import gene.Gene.{Individual, Population}
 
 object Selector {
-  def selection(estPop: List[(Individual, Int)], rng: RNG): (Population, RNG) = {
+  def selection (estPop: List[(Individual, Int)], rng: RNG): (Population, RNG) = {
     val half = estPop.size / 2
     val betterPop = estPop.sortWith((left, right) => left._2 > right._2).take(half).map(pair => pair._1)
     val restHalf = estPop.size - half
@@ -11,12 +11,14 @@ object Selector {
     (betterPop ++ arbitraryPop, newRng)
   }
 
+  def getArbitrary(population: Population, arbitratySize: Int): State[Long, Population] = ???
+
   //@todo: fix seed in RNG
-  def getArbitrary(pop: Population, arbitrarySize: Int, rng: RNG): (Population, RNG) =
-    if (pop.isEmpty || arbitrarySize == 0) (List(), rng)
+  def getArbitrary(population: Population, arbitrarySize: Int, rng: RNG): (Population, RNG) =
+    if (population.isEmpty || arbitrarySize == 0) (List(), rng)
     else {
-      val (result, currentRng) = getArbitrary(pop, arbitrarySize - 1, rng)
-      val (index, nextRng) = currentRng.nextIntLessThen(pop.size - 1)
-      (pop(index) :: result, nextRng)
+      val (result, currentRng) = getArbitrary(population, arbitrarySize - 1, rng)
+      val (index, nextRng) = currentRng.nextIntLessThen(population.size - 1)
+      (population(index) :: result, nextRng)
     }
 }
