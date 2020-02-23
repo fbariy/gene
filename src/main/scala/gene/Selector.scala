d7e7676428f7d9ff3f335275202502fd821bc851
fbariy <bariy.alex@gmail.com>
2020-02-23 13:34:31 +0300

    Got rid of trash

--- src/main/scala/gene/Selector.scala
+++ src/main/scala/gene/Selector.scala
@@ -1,36 +1,21 @@
 package gene
 
-import gene.Gene.{Individual, Population}
-
-import scala.annotation.tailrec
+import gene.Gene.{Individual, Population, PopulationEstimation}
 
 object Selector {
-  def selection (estPop: List[(Individual, Int)], rng: RNG): (Population, RNG) = {
-    val half = estPop.size / 2
-    val betterPop = estPop.sortWith((left, right) => left._2 > right._2).take(half).map(pair => pair._1)
-    val restHalf = estPop.size - half
-    val (arbitraryPop, newRng) = getArbitrary(estPop.map(pair => pair._1), restHalf, rng)
-    (betterPop ++ arbitraryPop, newRng)
-  }
+  def selection(estimator: Estimator)(population: Population): State[Long, PopulationEstimation] = {
+    val estimated = population.zip { population.map(estimator.estimate) }
+    val half = estimated.size / 2
+    val selected = estimated
+      .sortWith((current, next) => current._2 > next._2)
+      .take(half)
 
-  def selection(estimator: Estimator)(population: Population): State[Long, Population] = {
-    val estimationPopulation = population.zip { population.map(estimator.estimate) }
-    val half = estimationPopulation.size / 2
-    val betterPop = estimationPopulation.sortWith((left, right) => left._2 > right._2).take(half).map(pair => pair._1)
-    val restHalf = estimationPopulation.size - half
-    val (arbitraryPop, newRng) = getArbitrary(population, restHalf)
-    (betterPop ++ arbitraryPop, newRng)
-    ???
+    getArbitrary(estimated.size - half)(population)
+      .map(_.map(individual => (individual, estimator.estimate(individual))))
+      .map(selected ++ _)
   }
 
 
-  def getArbitrary(population: Population, arbitratySize: Int): State[Long, Population] = ???
-
-  def getArbitrary(population: Population, arbitrarySize: Int, rng: RNG): (Population, RNG) =
-    if (population.isEmpty || arbitrarySize == 0) (List(), rng)
-    else {
-      val (result, currentRng) = getArbitrary(population, arbitrarySize - 1, rng)
-      val (index, nextRng) = currentRng.nextIntLessThen(population.size - 1)
-      (population(index) :: result, nextRng)
-    }
+  def getArbitrary(individualSize: Int)(population: Population): State[Long, Population] =
+    State.sequence(List.fill(individualSize)(RNG2.lessThan(population.size).map(population(_))))
 }
