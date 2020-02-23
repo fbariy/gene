45c948612615659daaaba1ffd05f5c6205cf447b
fbariy <bariy.alex@gmail.com>
2020-02-23 20:03:56 +0300

    Got rid of trash in State

--- src/main/scala/gene/State.scala
+++ src/main/scala/gene/State.scala
@@ -1,16 +1,13 @@
 package gene
 
 case class State[S, A](run: S => (S, A)) {
-  def unit[B](a: B): State[S, B] =
-    State { s => (s, a) }
-
   def flatMap[B](f: A => State[S, B]): State[S, B] =
     State { s =>
       val (newS, a) = run(s)
       f(a).run(newS)
     }
 
-  def map[B](f: A => B): State[S, B] = flatMap { a => unit(f(a)) }
+  def map[B](f: A => B): State[S, B] = flatMap { a => State.unit(f(a)) }
 
   def map2[B, C](s: State[S, B])(f: (A, B) => C): State[S, C] =
     for {
@@ -27,18 +24,4 @@ object State {
     case List() => unit(List())
     case a :: rest => a.map2(sequence(rest))((a, b) => b :+ a)
   }
-
-  def both[S, A, B](s1: State[S, A], s2: State[S, B]): State[S, (A, B)] = for {
-    a <- s1
-    b <- s2
-  } yield (a, b)
-
-  def get[S]: State[S, S] = State { s => (s, s) }
-
-  def set[S](s: S): State[S, Unit] = State { _ => (s, ()) }
-
-  def modify[S](f: S => S): State[S, Unit] = for {
-    s <- get
-    _ <- set(f(s))
-  } yield ()
 }
